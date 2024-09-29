0b3b99da599035a9d5e8f1143cd7bf733559589d
STE <167364541+STE1717@users.noreply.github.com>
2024-09-29 15:30:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('guildMemberAdd', async (member) => {
             <a:whitearrow:1289791194318110761> <:heart:1289791508832190475> [Hexx](https://discord.com/channels/1275007722961764372/1280492854808019014)
             <a:whitearrow:1289791194318110761> <:heart:1289791508832190475> [events](https://discord.com/channels/1275007722961764372/1280492846075478018)
             
-            <a:heart:1289793488946331711> <a:flower:1289793837887393833> <a:heart:1289793488946331711> **Enjoy & stay w/us** <a:fire:1289615081725759619>
+            <a:heart:1289793488946331711> <a:flower:1289793837887393833> <a:heart:1289793488946331711> **Enjoy & stay w/us** <a:fire:1289615081725759619> 
             `)
             .setImage('https://media.discordapp.net/attachments/1280493004687282269/1289261167130181683/copy_F6827EA2-1EF8-4630-8C38-BEA4F77B14C2-ezgif.com-video-to-gif-converter.gif?ex=66fa27c7&is=66f8d647&hm=c49634dde8da94e053edfe28f6397f32934b4b8dd47c1bddb3e903e8eefac1cb') // Set the new banner image URL
             .setFooter({ 
